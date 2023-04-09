e815d7b44c5f67f9735cc7b9ffd331bbda6b9670
lukejohnsen <johnsen.luke@gmail.com>
2023-04-09 10:47:24 -0700

    useState and starting useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -23,11 +23,21 @@ const Home = () => {
       },
     ]);
 
-    return ( 
-        <div className="home">
-            <BlogList blogs={blogs}/>
-        </div>
-     );
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('useEffect ran');
+        console.log(blogs)
+    })
+
+    return (
+      <div className="home">
+        <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      </div>
+    );
 }
  
 export default Home;

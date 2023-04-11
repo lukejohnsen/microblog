635da59c0eb837df35066638aa1148919723563b
lukejohnsen <johnsen.luke@gmail.com>
2023-04-10 20:24:42 -0700

    creating custom useFetch hook, error handling

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,15 @@
-import { useState, useEffect } from "react";
+import useFetch from './useFetch';
 import BlogList from "./BlogList";
 
+
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-      {
-        title: "My new website",
-        body: "lorem ipsum...",
-        author: "mario",
-        id: 1,
-      },
-      {
-        title: "Welcome party!",
-        body: "lorem ipsum...",
-        author: "yoshi",
-        id: 2,
-      },
-      {
-        title: "Web dev top tips",
-        body: "lorem ipsum...",
-        author: "mario",
-        id: 3,
-      },
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    useEffect(() => {
-        console.log('useEffect ran');
-        console.log(blogs)
-    })
+const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs')
 
     return (
       <div className="home">
-        <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+        { error && <div>{error}</div> }
+        { isLoading && <div>Loading...</div> }
+        {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
       </div>
     );
 }
